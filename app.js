8408daf99c4684a181af4375b5eeee41c1b40b02
naxer-12 <rexan.2212@gmail.com>
2021-07-12 18:06:50 +0530

    added audio
    added audio file and its functionality
    fixed minor issue

---
+++ app.js
@@ -0,0 +1,42 @@
+// circularText();
+
+// function circularText() {
+
+//     let radius = 100
+//     let classIndex = 0
+//     const game_title = "mathemagic".toUpperCase();
+//     let txt = game_title.split("")
+//     classIndex = document.getElementsByClassName("game-title")[classIndex];
+
+//     let deg = 360 / txt.length
+//     let origin = -90;
+
+//     txt.forEach((ea) => {
+//         ea = `<h1 style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%;text-align:center;'>${ea}</h1>`;
+//         classIndex.innerHTML += ea;
+//         origin += deg;
+//     });
+// }
+
+//audio code
+
+const soundAction = (e) => {
+
+    console.log(e.target)
+    let sound_elements = document.getElementById('sound-section').children;
+
+    if (e.target === sound_elements[1]) {
+        sound_elements[0].removeAttribute("hidden")
+        sound_elements[1].setAttribute("hidden", true)
+        music.play();
+    } else if (e.target === sound_elements[0]) {
+        sound_elements[1].removeAttribute("hidden")
+        sound_elements[0].setAttribute("hidden", true)
+        music.pause();
+        music.currentTime = 0;
+    }
+}
+const music = new Audio("assets/music_bg.mp3")
+
+
+document.querySelector("#sound-section").addEventListener("click", soundAction)
